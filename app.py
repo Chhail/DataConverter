6ee21198f390f09050d5932cb770c16c13158050
Chhail <naidoochhail15@gmail.com>
2020-03-31 18:04:05 +0200

    Changes

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import xmltodict
 import json
 
-def xml_to_json():
-    with open('''<xml file name>.xml''') as xml_file:
+def xml_to_json(xml_file):
+    with open(xml_file) as xml_file:
         my_dict = xmltodict.parse(xml_file.read())
     xml_file.close()
     json_data = json.dumps(my_dict)
     print(json_data)
-
-
-
+    return json_data
