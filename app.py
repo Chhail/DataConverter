ca1c99ce73f39f92459e2bcf0dd97a23db4e1ba8
Chhail <naidoochhail15@gmail.com>
2020-03-31 21:23:16 +0200

    Changes

--- app.py
+++ app.py
@@ -2,24 +2,23 @@ import json
 import xmltodict
 
 def xml_to_json(xml_file):
-    with open(xml_file) as xml_file:
-        my_dict = xmltodict.parse(xml_file.read())
-    xml_file.close()
-    json_data = json.dumps(my_dict)
-    print(json_data)
-    return json_data
+    with open(xml_file) as f:
+        xmlString = f.read()
+
+    print("XML input (" + xml_file + "): \n" + xmlString)
+
+    jsonString = json.dumps(xmltodict.parse(xmlString), indent = 4)
+
+    print("\nJSON output (output.json): \n" + jsonString)
+
+    with open("output.json","w") as f:
+        f.write(jsonString)
 
 def json_to_xml(json_file):
-    with open(json_file) as f:
-        jsonString = f.read()
-
-    print("JSON input (" + json_file + "): \n" + jsonString)
-
-    xmlString = xmltodict.unparse(json.loads(jsonString), pretty = True)
-
+    with open(json_file) as json_file:
+        json_data = json_file.read()
+    print("JSON input (" + json_file + "): \n" + json_data)
+    xmlString = xmltodict.unparse(json.loads(json_data), pretty = True)
     print("\nXML output (output.xml): \n" + xmlString)
-
-    with open("output.xml", "w") as f:
-        f.write(xmlString)
-
-json_to_xml("my_json.json")
+    with open("output.xml", "w") as xml_file:
+        xml_file.write(xmlString)
