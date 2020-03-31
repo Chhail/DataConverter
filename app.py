d1923984d09fcddbc6c962517765300a369f52b4
Chhail <naidoochhail15@gmail.com>
2020-03-31 19:46:16 +0200

    Changes

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-import xmltodict
-import json
+import xmltodict as x
+import xml.etree.cElementTree as e
+import json as j
 
 def xml_to_json(xml_file):
     with open(xml_file) as xml_file:
-        my_dict = xmltodict.parse(xml_file.read())
+        my_dict = x.parse(xml_file.read())
     xml_file.close()
-    json_data = json.dumps(my_dict)
+    json_data = j.dumps(my_dict)
     print(json_data)
     return json_data
+
+def json_to_xml(json_file):
+    with open(json_file) as json_file:
+        d = j.load(json_file)
+
